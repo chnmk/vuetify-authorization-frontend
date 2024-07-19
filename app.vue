c85bd5277953b8d5a499aad2417942d568c38e8e
chnmk <a2molchanov@ya.ru>
2024-07-19 14:52:06 +0300

    feat: add full layout template

--- app.vue
+++ app.vue
@@ -1,34 +1,85 @@
 <template>
-  <!--
-    <div> 
-      <NuxtRouteAnnouncer />
-      <NuxtWelcome /> 
-    </div>
-  -->
   <v-app id="app">
-    <v-sheet class="mx-auto my-auto" width="300">
-      <v-form fast-fail @submit.prevent>
-        <v-text-field
-          v-model="username"
-          :rules="usernameRules"
-          label="Username"
-        ></v-text-field>
+    <v-card class="mx-auto my-auto" width="300">
+      <v-tabs class="ma-4" v-model="tab" align-tabs="center">
+        <v-tab value="signin">Sign In</v-tab>
+        <v-tab value="signup">Sign Up</v-tab>
+      </v-tabs>
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item value="signin">
+          <v-form fast-fail @submit.prevent>
+            <v-text-field
+              v-model="username"
+              :rules="usernameRules"
+              label="Username"
+            ></v-text-field>
 
-        <v-text-field
-          v-model="password"
-          :rules="passwordRules"
-          label="Password"
-        ></v-text-field>
+            <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              label="Password"
+            ></v-text-field>
 
-        <v-btn class="mt-2" type="submit" block @click="submit">Submit</v-btn>
-      </v-form>
-    </v-sheet>
+            <v-btn class="my-2" type="submit" block @click="submitSignin">Submit</v-btn>
+          </v-form>
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="signup">
+          <v-form fast-fail @submit.prevent>
+            <v-text-field
+              v-model="username"
+              :rules="usernameRules"
+              label="Username"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              label="Password"
+            ></v-text-field>
+
+            <v-select
+              label="Select group"
+              :rules="groupRules"
+              :items="['Group A', 'Group B']"
+            ></v-select>
+
+            <v-btn class="my-2" type="submit" block @click="submitSignup">Submit</v-btn>
+          </v-form>
+        </v-tabs-window-item>
+      </v-tabs-window>
+      <v-divider class="my-4" />
+      <v-card class="ma-4" color="primary">
+        <div class="d-flex justify-center">
+          <v-btn class="ma-2" width="150" @click="anyone">
+            Anyone
+          </v-btn>
+        </div>
+        <div class="d-flex justify-center">
+          <v-btn class="ma-2" width="150" @click="groupA">
+            Group A
+          </v-btn>
+        </div>
+        <div class="d-flex justify-center">
+          <v-btn class="ma-2" width="150" @click="groupB">
+            Group B
+          </v-btn>
+        </div>
+        <div class="d-flex justify-center">
+          <v-btn class="ma-2" width="150" @click="secret">
+            Secret
+          </v-btn>
+        </div>
+      </v-card>
+    </v-card>
   </v-app>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios';
+
+const tab = ref(null)
 
 const username = ref('')
 const usernameRules = [
@@ -62,7 +113,18 @@ value => {
   },
 ]
 
-function submit() {
+const groupRules = [
+value => {
+  if (value) return true
+  return 'Please select a group.'
+}
+]
+
+function submitSignin() {
+  console.log("signin placeholder")
+}
+
+function submitSignup() {
   axios.post('http://localhost:8080/signup', {
     username: username.value,
     password: password.value
@@ -73,5 +135,21 @@ function submit() {
   .catch(function (error) {
     console.log(error);
   });
+}
+
+function anyone() {
+  console.log("anyone placeholder")
+}
+
+function groupA() {
+  console.log("groupA placeholder")
+}
+
+function groupB() {
+  console.log("groupB placeholder")
+}
+
+function secret() {
+  console.log("secret placeholder")
 }
 </script>
